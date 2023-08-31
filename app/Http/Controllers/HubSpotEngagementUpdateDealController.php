714fc5880592a73a9628ef9aebe602601d5d0a0c
AndrewSamaev <andrey@hubstart.io>
2023-08-31 15:46:33 +0300

    API for notes is added, engagements deals update

--- app/Http/Controllers/HubSpotEngagementUpdateDealController.php
+++ app/Http/Controllers/HubSpotEngagementUpdateDealController.php
@@ -2,44 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use HubSpot\Factory;
+use HubSpot\Client\Crm\Deals\ApiException;
+use HubSpot\Client\Crm\Deals\Model\SimplePublicObjectInput;
 
 class HubSpotEngagementUpdateDealController extends Controller
 {
     public function createNoteInDeal(Request $request)
     {
-        $ticketId = env('HUBSPOT_DEAL_ID'); // Ticket ID to which the note will be added
-        $noteContent = "Content of the note";
+        $client = Factory::createWithAccessToken(env('HUBSPOT_API_KEY'));
 
-        $apiKey = env('HUBSPOT_API_KEY');
-        $url = "https://api.hubapi.com/crm-objects/v1/objects/deals/$ticketId";
-
-        $client = new Client();
-        $response = $client->get($url, [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $apiKey,
-                'Content-Type' => 'application/json',
-            ],
-            'json' => [
-                'objects' => [
-                    [
-                        'eventTypeId' => 'note',
-                        'header' => 'Note Added',
-                        'time' => time() * 1000, // Current time in milliseconds
-                        'content' => $noteContent,
-                    ],
-                ],
-            ],
+        $properties1 = [
+            'dealname' => 'Custom data integrations',
+        ];
+        $simplePublicObjectInput = new SimplePublicObjectInput([
+            'properties' => $properties1,
         ]);
-
-        $statusCode = $response->getStatusCode();
-        $responseData = json_decode($response->getBody(), true);
-
-        if ($statusCode === 200 && isset($responseData['status']) && $responseData['status'] === 'error') {
-            return response()->json(['error' => $responseData['message']], 400);
+        try {
+            $apiResponse = $client->crm()->deals()->basicApi()->update(8753139687, $simplePublicObjectInput);
+            echo $apiResponse;
+        } catch (ApiException $e) {
+            echo "Exception when calling basic_api->update: ", $e->getMessage();
         }
-
-        return response()->json($responseData, 200);
     }
 }
